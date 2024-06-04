0f854b71413e9a07170a98005c387d6e844945cd
sapnabhamdare123 <sapnabhamdare@gmail.com>
2024-06-04 16:07:05 +0530

    adding images

--- script.js
+++ script.js
@@ -3,21 +3,51 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input")
 const searchBtn = document.querySelector(".search button")
-// const weatherIcon = document.querySelector(".weather-icon")
+const weatherIcon = document.querySelector(".weather-icon")
 
 
 async function checkweather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
 
-    console.log(data);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        var data = await response.json();
 
-}
+        // console.log(data);
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "cloud.jpg";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "clear.jpg";
+    
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "rain.jpg";
+    
+        }
+    
+        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".error").style.display = "none";
+    
+    
+            
+    
+    }
+
+    }
+
+
+   
 
 searchBtn.addEventListener("click" ,()=>{
     checkweather(searchBox.value);
