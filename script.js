a7cbe1130f65b805a4f5a480680e86d752932c3d
sapnabhamdare123 <sapnabhamdare@gmail.com>
2024-06-04 14:58:43 +0530

    weather app

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey = "a004ceab0143afc59d180cf15c595df2";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+// const weatherIcon = document.querySelector(".weather-icon")
+
+
+async function checkweather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+}
+
+searchBtn.addEventListener("click" ,()=>{
+    checkweather(searchBox.value);
+    
+})
+
+
+
+
+
+
+
